ab717463aff3868d22c1433da1bbbaec03632fca
Flame <5017_30@mail.ru>
2023-04-01 14:18:11 +0300

    init: init project

---
+++ client/src/components/AIPicker.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const AIPicker = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default AIPicker
---
+++ client/src/components/FilePicker.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import CustomButton from './CustomButton'
+
+const FilePicker = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default FilePicker
